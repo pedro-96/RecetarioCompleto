2602c50404e5c208bfcf59799bcf88f3e2c09ffe
pedro-96 <95980964+pedro-96@users.noreply.github.com>
2024-09-16 16:51:55 -0300

    Update streamlit_app.py
    agregar imagenes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,43 +1,66 @@
 import streamlit as st
 import pandas as pd
 #from openpyxl import load_workbook, Workbook
-#import os
-#import time
+import os
+import time
 
 import gspread
 from google.oauth2.service_account import Credentials
 
 from io import BytesIO
+from PIL import Image
+import base64
 import xlsxwriter
+
+from openpyxl import Workbook
+from openpyxl.drawing.image import Image
 
 ### funcion descarga de excel
 def descargar_excel(dataframe):
     output = BytesIO()
     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
         dataframe.to_excel(writer, index=False, sheet_name='Sheet1')
+        #workbook =  writer.book
+        #worksheet = writer.sheets['Sheet1']
+        #worksheet.insert_image('N2', 'picture.png', {'foto': dataframe})
         #writer.save()  # No es necesario, pero se puede usar para asegurarse de que se escribe
+        #wb = Workbook()
+        #ws = wb.active
+        #for index, row in dataframe.iterrows():
+            #imagen = Image(BytesIO(row['miImage']))
+            #ws.add_image(imagen,f'B{index + 2}')
     processed_data = output.getvalue()
     return processed_data
+
+### función para convertir imagen a base64
+def convertir_imagen_a_base64(img):
+    buffered = BytesIO()
+    #img.save(buffered, format="PNG")
+    buffered.seek(0)
+    #base64.b64encode(buffered.getvalue()).decode()
+    return base64.b64encode(buffered.getvalue()).decode()
+
 
 ##### contrucción de la APP
 
 def CargaDosis():
     if "df" not in st.session_state:
         st.session_state.df = pd.DataFrame(columns=["Fecha de Receta","Campo",
-                                                    "Lote","Hectáreas del Lote",
+                                                    "Lote","Hectáreas totales Lote/s",
                                                     "Tipo de Insumo","Insumo",
                                                     "Insumo para agregar a la lista",
                                                     "Unidad de Medida","Cantidad por Lote",
                                                     "Dosis por Hectárea",
-                                                    "Persona Recetó","Observaciones"])
+                                                    "Persona Recetó","Observaciones", "Foto"])
+    
         
     st.title(f"ANIBAL BARBERO")
     #st.checkbox(f"A")
-    
+
     
     Campos = ["Gobbi", "San Miguel (Pascuet)", "Miloch","Pontel","Dandrea-Mores-Rossi-Capellari","Defassi","Ferrero"]  # Este será el input que determine la cantidad de tablas
     Lote = [1,2,3,4,5,6]
-    
+
     Tipo_Insumos = ["Herbicida", "Insecticida","Fungicida_Inoculante","Fertilizante","Semilla"]
         
     Herbicida =["2,4 D 100xciento","2,4 D enlist colexx-d x20lts","2,4 D Advance ethex-ester hf ac 68xciento x20lts","ACEITE agricola",
@@ -62,78 +85,89 @@ def CargaDosis():
     proveedor = ["Cotagro","Depetris","Dreyfus"]
     
     
-    unidad_de_medida = ["Litros","Mililitros","Kilos","Gramos"]
+    unidad_de_medida = ["Litros","Kilos","Gramos"]
     
     lotesConHectareas = {
             "Gobbi": {
-                "1 picadas": [53],
-                "2 entrando derecha": [64],
-                "3 atrás": [43]
+                "1 picadas": 53,
+                "2 entrando derecha": 64,
+                "3 atrás": 43
             },
             "San Miguel (Pascuet)": {
-                "1": [88],
-                "2": [92],
-                "3": [71],
-                "4": [66],
-                "5": [63],
-                "6": [87]
+                "1": 88,
+                "2": 92,
+                "3": 71,
+                "4": 66,
+                "5": 63,
+                "6": 87
             },
             "Miloch": {
-                "1 atrás del lote 2": [31],
-                "2 lado ruta (chico)": [28],
-                "3 lado ruta (grande)": [29],
-                "4 casa": [35]
+                "1 atrás del lote 2": 31,
+                "2 lado ruta (chico)": 28,
+                "3 lado ruta (grande)": 29,
+                "4 casa": 35
                 
             },
             "Pontel": {
-                "1 tapera": [50],
-                "2 G/pollos": [34.2],
-                "3 casa": [43.6],
-                "4 atras del 5": [50.4],
-                "5 lado ruta casa": [49.4]
+                "1 tapera": 50,
+                "2 G/pollos": 34.2,
+                "3 casa": 43.6,
+                "4 atras del 5": 50.4,
+                "5 lado ruta casa": 49.4
                 
             },
             "Dandrea-Mores-Rossi-Capellari": {
-                "1": [37],
-                "2 G/pollos": [37]
+                "1": 37,
+                "2 G/pollos": 37
                 
             },
             "Defassi": {
-                "1 entrada": [24],
-                "2 atrás": [25]
+                "1 entrada": 24,
+                "2 atrás": 25
                 
             },
             "Ferrero": {
-                "1": [30],
-                "2": [17],
-                "3": [34],
-                "4": [48],
-                "5": [30]
+                "1": 30,
+                "2": 17,
+                "3": 34,
+                "4": 48,
+                "5": 30
                 
             }
         }
-    
+
     # Defino las columnas que se utilizarán en la tabla
     columnas = ["Fecha de Retiro", "Campo", "Lote", "Proveedor", "Tipo de Insumo","Insumo",
                 "Insumo para agregar a la lista", "Tipo del insumo a agregar a la lista",
                 "Unidad de Medida", "Cantidad","Persona Retiró", "Persona Recibio",
                 "Persona Aplicó", "Persona Recetó" ,
                 "Persona que Pagará" ]
-    
-    
-    
+
+   
+
     
     st.header(f"Receta ")
     
     camp1 = st.selectbox(f"Campo ", options=list(lotesConHectareas.keys()), key=f"Camp_1")
-    FechaActual = st.date_input(f"Fecha")
+    FechaActual = st.date_input(f"Fecha", format="DD/MM/YYYY")
+
+    #dfcheck = pd.DataFrame(lotesConHectareas[camp1])
+    #st.dataframe(dfcheck)
+    #check = st.checkbox(f'{list(dfcheck.keys())}')
+
+    lote = st.multiselect(f"Lote", options=lotesConHectareas[camp1].keys(), key=f"Lote_1")
     
-    lote = st.selectbox(f"Lote", options=list(lotesConHectareas[camp1].keys()), key=f"Lote_1")
-    
-    hectateras_lote = st.selectbox(f"Hectáreas lote {lote}", options=lotesConHectareas[camp1][lote], key=f"Hectáreas_1", disabled= True)
-    
+    Total_hectareas_por_lotes_seleccionados = 0
+    for camp, lot in lotesConHectareas.items():
+        for lo in lote:
+            if lo in lot:
+                Total_hectareas_por_lotes_seleccionados = Total_hectareas_por_lotes_seleccionados +lot[lo]
+
+    #hectateras_lote = st.selectbox(f"Hectáreas lote {lote}", options=lotesConHectareas[camp1][lote], key=f"Hectáreas_1", disabled= True)
+    hectateras_lote = st.selectbox(f"Hectáreas lote/s {lote}", options=[str(Total_hectareas_por_lotes_seleccionados)], key=f"Hectáreas_1", disabled= True)
+
     Tinsumo = st.selectbox(f"Tipo de Insumo", options= Tipo_Insumos, key=f"Tipo_Insumos_1")
-    
+
     # Genero inputs en Streamlit anidados 
     Insumo = st.selectbox(f"{Tinsumo} para {camp1}. *SI no lo encontrás elige la opción OTRO* ",
                         Herbicida if Tinsumo == "Herbicida" else 
@@ -148,49 +182,85 @@ def CargaDosis():
         agregar_este_insumo = st.text_input(f"Escribe el nombre del Insumo que no encuentras", None)
     else:
         pass
-    
+
     # Inputs sin anidar
     Unidad_de_Medida = st.selectbox(f"Unidad de medida", options=unidad_de_medida, key=f"unidad_de_medida_1")
-    Cantidad = st.number_input(f"Cantidad TOTAL de {Unidad_de_Medida} para lote {lote}", min_value=1)#, min_value=0
-    Dosis_por_hectarea = Cantidad/hectateras_lote
-    Cantidadhc = st.text_input(f"Dosis Por Hectárea de {Insumo}", value=str(Dosis_por_hectarea), disabled= True)
+
+    #Cantidad = st.number_input(f"Cantidad TOTAL de {Unidad_de_Medida} para lote {lote}", min_value=1)#, min_value=0
+    Dosis_por_hectarea = st.number_input(f"Dosis Por Hectárea de {Insumo}", min_value=0.0000)
+    cantidad_total = Dosis_por_hectarea*Total_hectareas_por_lotes_seleccionados
+    Cantidad = st.text_input(f"Cantidad TOTAL de {Unidad_de_Medida} para lote/s {lote}", value=str(cantidad_total), disabled= True)
     #Precio = st.number_input(f"Precio Unitario en USD (*Sin IVA*) del insumo para {camp1}") #, min_value=0
     Persona_Recetó = st.text_input(f"Persona que Recetó ", None, key=f"Persona_{camp1}_3")
     Observaciones = st.text_input(f"Observación", None, key=f"Persona_{camp1}_31")
+
+
     
+    fileName = None
+    
+    if 'miImage' not in st.session_state.keys():
+        st.session_state['miImage'] = None
+    picture = st.camera_input("foto")
+
+    if picture is not None:
+        st.session_state['miImage'] = picture
+
+    if st.session_state['miImage']:
+        #abrirr_imagen = Image.open(st.session_state['miImage'])
+        st.image(st.session_state['miImage'], caption="Imagen capturada", use_column_width=True)
+        
+        fileName = f'Imagen_Para_{camp1}_Fecha_{FechaActual}_lotes:{lote}{st.session_state['miImage'].name}'
+        #saveButton = st.button('guardar imagen')
+        
+        
+        #if saveButton:
+            #with open(fileName, "wb") as imageFile:
+                #imageFile.write(st.session_state['miImage'].getbuffer())
+                #st.success('Imagen guardada correctamente')
+            
+        bytes_data = fileName
+        #img = open(bytes_data)
+        #st.image(img, caption="imagen capturada")
+        #convertir_imagen_a_base64(abrirr_imagen)
+        #picture.getvalue()
+     
+
+
     # Creo el DataFrame a partir de los inputs
-    
+
     if agregar_este_insumo == "":
         data = {
-            "Fecha de Receta": [FechaActual],
-            "Campo": [camp1],
-            "Lote": [lote],
-            "Hectáreas del Lote": [hectateras_lote],
-            "Tipo de Insumo": [Tinsumo],
-            "Insumo": [Insumo],
-            "Insumo para agregar a la lista": [None],
-            "Unidad de Medida":[Unidad_de_Medida],
-            "Cantidad por Lote": [Cantidad],
+            "Fecha de Receta": FechaActual,
+            "Campo": camp1,
+            "Lote": lote,
+            "Hectáreas totales Lote/s": hectateras_lote,
+            "Tipo de Insumo": Tinsumo,
+            "Insumo": Insumo,
+            "Insumo para agregar a la lista": None,
+            "Unidad de Medida":Unidad_de_Medida,
+            "Cantidad por Lote": Cantidad,
             #"Cantidad por Hectárea": [Cantidadhc],
-            "Dosis por Hectárea": [Dosis_por_hectarea],
-            "Persona Recetó" :[Persona_Recetó],
-            "Observaciones":[Observaciones]
+            "Dosis por Hectárea": Dosis_por_hectarea,
+            "Persona Recetó" :Persona_Recetó,
+            "Observaciones":Observaciones,
+            "Foto": fileName
             }
     else:
         data = {
-            "Fecha de Receta": [FechaActual],
-            "Campo": [camp1],
-            "Lote": [lote],
-            "Hectáreas del Lote": [hectateras_lote],
-            "Tipo de Insumo": [Tinsumo],
-            "Insumo": [Insumo],
-            "Insumo para agregar a la lista": [agregar_este_insumo],
-            "Unidad de Medida":[Unidad_de_Medida],
-            "Cantidad por Lote": [Cantidad],
+            "Fecha de Receta": FechaActual,
+            "Campo": camp1,
+            "Lote": lote,
+            "Hectáreas totales Lote/s": hectateras_lote,
+            "Tipo de Insumo": Tinsumo,
+            "Insumo": Insumo,
+            "Insumo para agregar a la lista": agregar_este_insumo,
+            "Unidad de Medida":Unidad_de_Medida,
+            "Cantidad por Lote": Cantidad,
             #"Cantidad por Hectárea": [Cantidadhc],
-            "Dosis por Hectárea": [Dosis_por_hectarea],
-            "Persona Recetó" :[Persona_Recetó],
-            "Observaciones":[Observaciones]
+            "Dosis por Hectárea": Dosis_por_hectarea,
+            "Persona Recetó" :Persona_Recetó,
+            "Observaciones": Observaciones,
+            "Foto": fileName
             }
     
     #df = pd.DataFrame(data)
@@ -199,35 +269,37 @@ def CargaDosis():
     # Creo una nueva fila con los valores capturados
         if agregar_este_insumo == "":
             nueva_fila = {
-                "Fecha de Receta": [FechaActual],
-                "Campo": [camp1],
-                "Lote": [lote],
-                "Hectáreas del Lote": [hectateras_lote],
-                "Tipo de Insumo": [Tinsumo],
-                "Insumo": [Insumo],
-                "Insumo para agregar a la lista": [None],
-                "Unidad de Medida":[Unidad_de_Medida],
-                "Cantidad por Lote": [Cantidad],
+                "Fecha de Receta": FechaActual,
+                "Campo": camp1,
+                "Lote": lote,
+                "Hectáreas totales Lote/s": hectateras_lote,
+                "Tipo de Insumo": Tinsumo,
+                "Insumo": Insumo,
+                "Insumo para agregar a la lista": None,
+                "Unidad de Medida":Unidad_de_Medida,
+                "Cantidad por Lote": Cantidad,
                 #"Cantidad por Hectárea": [Cantidadhc],
-                "Dosis por Hectárea": [Dosis_por_hectarea],
-                "Persona Recetó" :[Persona_Recetó],
-                "Observaciones":[Observaciones]
+                "Dosis por Hectárea": Dosis_por_hectarea,
+                "Persona Recetó" :Persona_Recetó,
+                "Observaciones":Observaciones,
+                "Foto": fileName
                 }
         else:
             nueva_fila = {
-                "Fecha de Receta": [FechaActual],
-                "Campo": [camp1],
-                "Lote": [lote],
-                "Hectáreas del Lote": [hectateras_lote],
-                "Tipo de Insumo": [Tinsumo],
-                "Insumo": [Insumo],
-                "Insumo para agregar a la lista": [agregar_este_insumo],
-                "Unidad de Medida":[Unidad_de_Medida],
-                "Cantidad por Lote": [Cantidad],
+                "Fecha de Receta": FechaActual,
+                "Campo": camp1,
+                "Lote": lote,
+                "Hectáreas totales Lote/s": hectateras_lote,
+                "Tipo de Insumo": Tinsumo,
+                "Insumo": Insumo,
+                "Insumo para agregar a la lista": agregar_este_insumo,
+                "Unidad de Medida":Unidad_de_Medida,
+                "Cantidad por Lote": Cantidad,
                 #"Cantidad por Hectárea": [Cantidadhc],
-                "Dosis por Hectárea": [Dosis_por_hectarea],
-                "Persona Recetó" :[Persona_Recetó],
-                "Observaciones":[Observaciones]
+                "Dosis por Hectárea": Dosis_por_hectarea,
+                "Persona Recetó" :Persona_Recetó,
+                "Observaciones":Observaciones,
+                "Foto": fileName
                 }
         
         # Agrego nueva fila al DataFrame con append
@@ -237,18 +309,22 @@ def CargaDosis():
     SDF = st.session_state.df
     st.write(f"Controlar Datos Cargados!!")
     st.write(SDF)
-    
-    
+
+
         ## boton para descargar datos ya guardados en un xlsl
-    
+
     st.download_button(
         label="Descargar Receta en Excel",
         data=descargar_excel(pd.DataFrame(SDF)),
         file_name=f'Receta_Para_{camp1}_Fecha_{FechaActual}.xlsx',
         mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
         )
+    if st.session_state['miImage']:
+        st.download_button('Descacargar foto',  data=st.session_state['miImage'], file_name=f'Imagen_Para_{camp1}_Fecha_{FechaActual}.jpg')
+    
         
 
 if __name__ == "__main__":
     CargaDosis()
+    
     
